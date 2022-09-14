51abf1a425236294f2e734403d333a2a68463461
Andy-Home <1amajfranck@gmail.com>
2022-09-13 17:54:17 -0700

    condensed

--- algorithms/header.h
+++ algorithms/header.h
@@ -8,7 +8,20 @@ using namespace std;
 
 
 
-vector<string> loadData(string fileName);
+vector<string> loadData(string fileName)
+{
+    vector<string> data;
+    ifstream infile;
+    infile.open(fileName);
+    
+    string temp;
+    while(!infile.eof())
+    {
+        getline(infile, temp);
+        data.push_back(temp);
+    }
+    return data;
+}
 
 
 bool mergeSort(vector<string> &data);
